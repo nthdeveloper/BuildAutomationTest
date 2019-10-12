0b869a6ece86f43b27f33e60482993438b4d5ed0
nthdeveloper <mustafakok@gmail.com>
2019-10-12 11:32:17 +0200

    Add fake unit test.

---
+++ WinApp.Tests/WinAppTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WinApp.Tests
+{
+    [TestFixture]
+    public class WinAppTests
+    {
+        [Test]
+        public void SampleTestMethod()
+        {
+            //Fake test to be executed by CI/CD tool
+            Assert.That(true, Is.True);
+        }
+    }
+}
